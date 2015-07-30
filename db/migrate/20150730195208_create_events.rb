204f64d70dd4f889535390b8e13a9f3f6c4e0dca
Shayde Nofziger <shayde.nofziger@blackbaud.com>
2015-07-30 16:08:16 -0400

    using correct sql types

--- db/migrate/20150730195208_create_events.rb
+++ db/migrate/20150730195208_create_events.rb
@@ -2,12 +2,12 @@ class CreateEvents < ActiveRecord::Migration
   def change
     create_table :events do |t|
       t.string :name
-      t.DateTime :start
-      t.DateTime :end
+      t.timestamp :start
+      t.timestamp :end
       t.string :location
       t.string :info
       t.string :picture
-      t.int :maxVolunteers
+      t.integer :maxVolunteers
       t.boolean :active
 
       t.timestamps null: false
