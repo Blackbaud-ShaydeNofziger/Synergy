e14c309ffaa489eb6ee214d8e895bc3cd984f719
Blackbaud-EmmaRastatter <emma.rastatter@blackbaud.com>
2015-07-30 22:52:14 -0400

    Revert "Added show in user controller"
    This reverts commit 38f6cb4a979966527be1ab2cc8d7be36afc28e57.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -54,6 +53,4 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-  
-  get '/users/:id', to: 'users#show'
 end
