b8d840d5360e8931cf1f78f3b0ca92d3b893c986
Shayde Nofziger <shayde.nofziger@blackbaud.com>
2015-07-31 05:00:34 -0400

    tabs to spaces

--- public/javascript/routes.js
+++ public/javascript/routes.js
@@ -1,38 +1,38 @@
 var synergy = angular.module('synergy',[
-	'ngRoute',
-	'synergyControllers'
+  'ngRoute',
+  'synergyControllers'
 ]);
 
 synergy.config(['$routeProvider',
-	function($routeProvider)	{
-		$routeProvider.
-			when('/', {
-				templateUrl: '/templates/sign-in.html',
-				controller: 'signInController'
-			}).
-			when('/hello', {
-				templateUrl: '/templates/hello.html',
-				controller: 'indexController'
-			}).
-			when('/create_event', {
-				templateUrl: '/templates/create_event.html',
-				controller: 'createEventController'
-			}).
-			when('/bye', {
-				templateUrl: '/templates/bye.html',
-				controller: 'IndexController'
-			}).
-			when('/profile', {
-				templateUrl: '/templates/profile.html',
-				controller: 'ProfileController'
-			}).
-			when('/organization/create', {
-				templateUrl: '/templates/create_organization.html',
-				controller: 'OrganizationController'
-			}).
-			when('/events/browse', {
-				templateUrl: '/templates/browse_events.html',
-				controller: 'BrowseEventsController'
-			});
-	}
+  function($routeProvider)  {
+    $routeProvider.
+      when('/', {
+        templateUrl: '/templates/sign-in.html',
+        controller: 'signInController'
+      }).
+      when('/hello', {
+        templateUrl: '/templates/hello.html',
+        controller: 'indexController'
+      }).
+      when('/create_event', {
+        templateUrl: '/templates/create_event.html',
+        controller: 'createEventController'
+      }).
+      when('/bye', {
+        templateUrl: '/templates/bye.html',
+        controller: 'IndexController'
+      }).
+      when('/profile', {
+        templateUrl: '/templates/profile.html',
+        controller: 'ProfileController'
+      }).
+      when('/organization/create', {
+        templateUrl: '/templates/create_organization.html',
+        controller: 'OrganizationController'
+      }).
+      when('/events/browse', {
+        templateUrl: '/templates/browse_events.html',
+        controller: 'BrowseEventsController'
+      });
+  }
 ]);
