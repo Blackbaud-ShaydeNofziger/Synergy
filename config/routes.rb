860d0d13d0cdeb1631437fd3c8ebefb7988d541e
Blackbaud-RyanBecwar <ryan.becwar@blackbaud.com>
2015-07-30 22:57:05 -0400

    Added show method in user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
   root to: 'application#main'
+
+  post '/create' => 'organizations#create'
+
+  get '/user/:id' => 'users#show'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
