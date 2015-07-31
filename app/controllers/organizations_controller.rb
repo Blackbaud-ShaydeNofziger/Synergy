e8e512969a6d8b04d40e9d2109d042fb48dbc7ec
Blackbaud-RyanBecwar <ryan.becwar@blackbaud.com>
2015-07-31 02:01:49 -0400

    Whitespace

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -2,13 +2,18 @@ class OrganizationsController < ApplicationController
 
 	# POST
 	def create
-		org = Organizaton.new(name:    params[:name],
-													website: params[:message],
-													phone:   params[:phone],
-													info:    params[:info],
-													address: params[:info])
-		org.save
-		render nothing: true, status: 200
+
+		if(params.has_key?(:name))
+		  org = Organizaton.new(name:    params[:name],
+		                        website: params[:message],
+  													phone:   params[:phone],
+  													info:    params[:info],
+  													address: params[:info])
+  		org.save
+  		render nothing: true, status: 200
+		else
+			render nothing: true, status: 500
+		end
 	end
 
 	# GET
