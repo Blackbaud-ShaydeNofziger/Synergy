369a01566f3bb691e8d841715a3874eb54b80a1c
Blackbaud-RyanBecwar <ryan.becwar@blackbaud.com>
2015-07-31 09:28:21 -0400

    sdfsdf

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,23 @@
 class UsersController < ApplicationController
 
 	# For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
-  
+  protect_from_forgery with: :null_session
+
 	# POST
 	def volunteerAtEvent
 	end
 
 	# GET
 	def id
+    name = params[:name]
+    #@ev = Event.find_by name: params[:name]
+    #return render json: @ev.id
+    for user in User.all
+      if user.name == name
+        break
+      end
+    end
+    return render json: user.id
 	end
 
 	#GET
@@ -17,9 +26,12 @@ class UsersController < ApplicationController
 		@user = User.find_by_id(id)
 		render json: @user
 	end
-	
+
 	# POST
 	def update
+    id = params[:id]
+    @user = User.find_by_id(id)
+
 	end
 
 	# POST
